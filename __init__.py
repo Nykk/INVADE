acc636305f79f90be9f9b2a7e7d50e46c3b74a60
medvosa <Pr0cheen>
2020-03-31 01:49:42 +0300

    working statistics about word

--- __init__.py
+++ __init__.py
@@ -215,6 +215,7 @@ def gwi():
         word_object = session.query(Word).filter_by(word_set=word_set.id, spelling=word).first()
         print(word)
         # print(dir(request))
+        print(word_object.train1)
         return word_object.translation+'; '+str(word_object.trains)[1:-1]
         abort(409)
     return 'not logged in'
