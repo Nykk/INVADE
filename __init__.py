9e418b36dd8e89ab9ed1ad4e038866001031a8d0
medvosa <medvosa@yandex.ru>
2020-06-08 23:26:04 +0300

    test git

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import googletrans
 from flask import Flask, request, session as ses, render_template, redirect, abort
 from sqlalchemy import create_engine, MetaData
+
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 print()
